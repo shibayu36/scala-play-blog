fd8354c9529c9081888fafaa42de09716cd361e9
shibayu36 <shibayu36@gmail.com>
2017-09-02 21:32:13 +0900

    Add tests to check html by jsoup

--- build.sbt
+++ build.sbt
@@ -9,6 +9,7 @@ scalaVersion := "2.12.2"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.0" % Test
+libraryDependencies += "org.jsoup" % "jsoup" % "1.10.3" % Test
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "org.shibayu36.controllers._"
--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -2,6 +2,7 @@ package controllers
 
 import play.api.test._
 import play.api.test.Helpers._
+import org.jsoup.Jsoup
 
 /**
  * Add your spec here.
@@ -18,6 +19,27 @@ class HomeControllerSpec extends test.ControllerSpec {
       status(home) shouldBe OK
       contentType(home) shouldBe Some("text/html")
       contentAsString(home) should include ("Welcome to Play")
+
+      val doc = Jsoup.parse(contentAsString(home))
+
+      val h1 = doc.select("h1")
+      h1.text shouldBe "Welcome to Play!"
+
+      val pElems = doc.select("p")
+      pElems.get(0).text shouldBe "hoge1"
+      pElems.get(1).text shouldBe "fuga1"
+      pElems.get(2).text shouldBe "hoge2"
+      pElems.get(3).text shouldBe "fuga2"
+
+      val pWithClass1Elems = doc.select("p.class1")
+      pWithClass1Elems.get(0).text shouldBe "hoge1"
+      pWithClass1Elems.get(1).text shouldBe "hoge2"
+
+      // nested example
+      val div1Elem = doc.select(".div1").first
+      val pElemsInDiv1 = div1Elem.select("p")
+      pElemsInDiv1.get(0).text shouldBe "hoge1"
+      pElemsInDiv1.get(1).text shouldBe "fuga1"
     }
   }
 }
